e65c3321203e5942bc3e6e8ef275a8c404b3d9f9
Tatsuro BABA <t.baba@grooves.co.jp>
2010-07-23 08:51:00 +0900

    using devise 1.1.rc2

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'arel',  :git => 'git://github.com/rails/arel.git'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
 # Use devise as a Authrozation
-gem 'devise'
+gem 'devise', '1.1.rc2'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Gamma
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Using encryptor 'sha1' for Devise.
+    config.encryptor = :sha1
   end
 end
