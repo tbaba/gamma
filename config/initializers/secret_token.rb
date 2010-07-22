0745b7c53685d722aa28637bcaee9b45f1f4d02d
Tatsuro BABA <t.baba@grooves.co.jp>
2010-07-23 08:39:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamma::Application.config.secret_token = 'ce6f266fa54034b4a826842936f8c7f277b18fe78c1e0cb26d484399060a6023071c1db630901ea0ff9997f9ee22cf366122dc411aff8be333196f3838bec8e3'
